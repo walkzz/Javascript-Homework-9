9b0a22cd0b636bc4ccba84a57996942dc53a1692
Pavel Toshinski <pavelce2000@hotmail.com>
2024-06-04 23:16:21 +0200

    Added class Rectangle with setters and getters plus 3 methods: getShapeType,  getArea and describe

--- exercise-2.js
+++ exercise-2.js
@@ -10,3 +10,49 @@
 // Метод describe() koj што ќе ги принта бојата, ширината и висината на правоаголникот.
 // Да се направат неколку инстанци од оваа класа и да се употребат соодветните методи.
 
+class Rectangle {
+    #width;
+    #height;
+
+    constructor(color, width, height) {
+        this.color = color;
+        this.#width = width;
+        this.#height = height;
+    }
+
+    static getShapeType() {
+        return "Rectangle";
+    }
+
+    get width() {
+        return this.#width;
+    }
+
+    set width(value) {
+        if (value > 0) {
+            this.#width = value;
+        } else {
+            console.error("Width must be a positive number!");
+        }
+    }
+
+    get height() {
+        return this.#height;
+    }
+
+    set height(value) {
+        if (value > 0) {
+            this.#height = value;
+        } else {
+            console.error("Height must be a positive number!");
+        }
+    }
+
+    getArea() {
+        return this.#width * this.#height;
+    }
+
+    describe() {
+        console.log(`Color: ${this.color}, Width: ${this.#width}, Height: ${this.#height}`);
+    }
+}
